28d04c0e5f918979be48f5c3e2faa50bab959156
h-latta <h.latta@yahoo.com>
2022-02-15 11:03:34 -0600

    Added Admin Control for Products, Added POST Function to Views File

--- products/views.py
+++ products/views.py
@@ -2,11 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Products
 from .serializers import ProductSerializer
+from rest_framework import status
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Products.objects.all()
-    serializer = ProductSerializer(products, many=True)
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
