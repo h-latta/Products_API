f434cbdc2126e91cb380ad9fff4785976efbdbbc
h-latta <h.latta@yahoo.com>
2022-02-15 10:47:54 -0600

    Created GET Function to Pull Product List

--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import Products
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
 def product_list(request):
-    return Response('ok')
+    products = Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
